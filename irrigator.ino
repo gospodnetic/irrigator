cde26eaf4dd3ca65f2313dff7587c60499af1ece
Petra Gospodnetic <petra.gospodnetic@itwm.fraunhofer.de>
2022-09-10 23:39:22 +0200

    Prepare calibrate functions and refactor main into functions

--- irrigator.ino
+++ irrigator.ino
@@ -18,47 +18,16 @@ bool keyup = true;
 #define RELAY 2
 #define BUTTON 8
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  lcd.begin(16, 2);
-  pinMode(RELAY, OUTPUT);
-  pinMode(BUTTON, INPUT_PULLUP);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
+void run_irrigator()
+{
   moist_val = analogRead(A0);
-//  Serial.println(moist_val);
-
-  int button = digitalRead(BUTTON);
-  if (button == 0 && keyup)
-  {
-    keydown = true;
-    keyup = false;
-
-    system_mode = system_mode + 1 < 3
-      ? system_mode + 1
-      : 0;
-  }
-  else if (button == 1 && keydown)
-  {
-    keyup = true;
-    keydown = false;  
-  }
-  Serial.println(button);
-  Serial.println(system_mode);
-
+  
   moist_percent = map(moist_val, dry_val, wet_val, 0 , 100);
   if (moist_percent < 0)
     moist_percent = 0;
   else if (moist_percent > 100)
     moist_percent = 100;
-
-  // Print to serial.
-//  Serial.print(moist_percent);
-//  Serial.println("%");
-
+    
   // Print to LCD.
   lcd.setCursor(0, 0);
   lcd.print("Moisture");
@@ -80,4 +49,61 @@ void loop() {
   delay(250);
 
   lcd.clear();
+}
+
+void read_button()
+{
+  int button = digitalRead(BUTTON);
+  if (button == 0 && keyup)
+  {
+    keydown = true;
+    keyup = false;
+
+    system_mode = system_mode + 1 < 3
+      ? system_mode + 1
+      : 0;
+  }
+  else if (button == 1 && keydown)
+  {
+    keyup = true;
+    keydown = false;  
+  }
+  Serial.println(button);
+  Serial.println(system_mode);
+}
+
+void calibrate_wet()
+{
+  
+}
+
+void calibrate_dry()
+{
+  
+}
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  pinMode(RELAY, OUTPUT);
+  pinMode(BUTTON, INPUT_PULLUP);
+}
+
+void loop()
+{
+  read_button();
+  switch (system_mode)
+  {
+    case 1:
+      calibrate_dry();
+      break;
+    case 2:
+      calibrate_wet();
+      break;
+    default:
+      run_irrigator();
+      break;
+  }
 }
